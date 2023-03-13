9381c6144550f6bcc431bd8bad2c35fb61413a75
anhaki <m.anugrahhakiki@gmail.com>
2023-03-13 09:13:49 +0700

    Menambahkan adapter

--- app/src/main/java/com/example/madun_haki/MyDatabaseHelper.java
+++ app/src/main/java/com/example/madun_haki/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.madun_haki;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -50,5 +51,15 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
         long eksekusi = db.insert(TABLE_NAME,null,cv);
         return eksekusi;
+    }
+
+    public Cursor bacaDataPlayer(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor varCursor = null;
+        if(db != null){
+            varCursor = db.rawQuery(query, null);
+        }
+        return varCursor;
     }
 }
